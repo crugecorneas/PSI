e7f8e181678800af5b70d74f53fa0a69e2bfc141
freddys <freddysz@gmail.com>
2025-02-05 18:47:41 +0100

    administrador django

--- Practica1/catalog/admin.py
+++ Practica1/catalog/admin.py
@@ -9,10 +9,13 @@ admin.site.register(Genre)
 #admin.site.register(BookInstance)
 admin.site.register(Language)
 
+class BooksInline(admin.TabularInline):
+    model = Book
 # Define the admin class
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BooksInline]
 
 # Register the admin class with the associated model
 admin.site.register(Author, AuthorAdmin)
@@ -29,6 +32,7 @@ class BookAdmin(admin.ModelAdmin):
 
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'due_back', 'id')
     list_filter = ('status', 'due_back')
 
     fieldsets = (
