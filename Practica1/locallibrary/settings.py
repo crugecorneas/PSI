44107b9a3f93004e391b386d5fffee0e8b68a4ef
Iñaki <inakidelafuente@gmail.com>
2025-02-06 17:27:37 +0100

    Parte

--- Practica1/locallibrary/settings.py
+++ Practica1/locallibrary/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -85,8 +86,8 @@ DATABASES = {
     }
 }
 
-import dj_database_url
-db_from_env= dj_database_url.config(default='postgres://alumnodb:alumnodb@localhost:5432/psi',conn_max_age=500)
+db_from_env = dj_database_url.config(
+    default='postgres://alumnodb:alumnodb@localhost:5432/psi', conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 # Password validation
