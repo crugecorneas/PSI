e3f81c0fa7a8a7539bb4013dfda5475d6ce22a8e
freddys <freddysz@gmail.com>
2025-02-17 21:08:23 +0100

    .build.sh

--- Practica1/build.sh
+++ Practica1/build.sh
@@ -1,25 +1,6 @@
-#!/bin/bash
+set -o errexit  # exit on error
 
-# Salir si ocurre un error
-set -e
+pip install -r requirements.txt
 
-# Cargar variables de entorno desde el archivo .env
-if [ -f .env ]; then
-    export $(grep -v '^#' .env | xargs)
-fi
-
-# Aplicar migraciones de la base de datos
-echo "Aplicando migraciones..."
+python manage.py collectstatic --no-input
 python manage.py migrate
-
-# Crear superusuario si no existe
-echo "Creando superusuario..."
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); \
-if not User.objects.filter(username='alumnodb').exists(): \
-    User.objects.create_superuser('alumnodb', 'admin@example.com', 'alumnodb')" | python manage.py shell
-
-# Poblar la base de datos si es necesario
-echo "Poblando la base de datos..."
-python manage.py loaddata initial_data.json
-
-echo "Configuración completa."
