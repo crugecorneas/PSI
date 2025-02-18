9543ccb1e613cf41440cbbbb24294156f7b8b885
freddys <freddysz@gmail.com>
2025-02-18 16:09:00 +0100

    build.sh

--- build.sh
+++ build.sh
@@ -8,6 +8,10 @@ cd Practica1
 python manage.py collectstatic --no-input
 python manage.py migrate
 
+echo "Creando superusuario de Django..."
+python manage.py createsuperuser --no-input || echo "El superusuario ya existe."
+
+
 # Verifica si la BD está vacía antes de poblarla
 if ! python manage.py shell -c "from catalog.models import Book; exit(0 if Book.objects.exists() else 1)"; then
     echo "La base de datos está vacía. Ejecutando populate_catalog.py..."
