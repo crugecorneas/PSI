85b84302ef801da0c95b553c344382412d43a065
freddys <freddysz@gmail.com>
2025-02-18 15:58:00 +0100

    build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -o errexit  # exit on error
+set -o errexit  # Exit on error
 
 pip install -r requirements.txt
 
@@ -7,6 +7,14 @@ cd Practica1
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Verifica si el usuario 'alumnodb' existe en PostgreSQL
+if ! sudo -u postgres psql -tAc "SELECT 1 FROM pg_roles WHERE rolname='alumnodb'" | grep -q 1; then
+    echo "El usuario 'alumnodb' no existe. Creándolo..."
+    sudo -u postgres psql -c "CREATE ROLE alumnodb WITH SUPERUSER LOGIN PASSWORD 'alumnodb';"
+else
+    echo "El usuario 'alumnodb' ya existe."
+fi
 
 # Verifica si la BD está vacía antes de poblarla
 if ! python manage.py shell -c "from catalog.models import Book; exit(0 if Book.objects.exists() else 1)"; then
