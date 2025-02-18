37a9c3a03bc1eb009bc6fb2a4f97e5c815fa4665
freddys <freddysz@gmail.com>
2025-02-18 09:21:55 +0100

    I

--- build.sh
+++ build.sh
@@ -3,5 +3,9 @@ set -o errexit  # exit on error
 pip install -r requirements.txt
 
 cd Practica1
+
+
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+python3 populate_catalog.py 
