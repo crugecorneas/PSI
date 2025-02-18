ec90703efe624640bdd2dd8b75ebbccb891b3467
freddys <freddysz@gmail.com>
2025-02-18 20:11:03 +0100

    create superuser

--- build.sh
+++ build.sh
@@ -8,14 +8,20 @@ cd Practica1
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-echo "Creando superusuario de Django..."
-python manage.py createsuperuser --no-input || echo "El superusuario ya existe."
+echo "Verificando si el superusuario alumnodb ya existe..."
+EXISTING_USER=$(echo "from django.contrib.auth import get_user_model; print(get_user_model().objects.filter(username='alumnodb').exists())" | python manage.py shell)
 
+if [ "$EXISTING_USER" = "True" ]; then
+    echo "El superusuario alumnodb ya existe."
+else
+    echo "Creando superusuario de Django..."
+    python manage.py createsuperuser --no-input
+fi
 
 # Verifica si la BD está vacía antes de poblarla
 if ! python manage.py shell -c "from catalog.models import Book; exit(0 if Book.objects.exists() else 1)"; then
     echo "La base de datos está vacía. Ejecutando populate_catalog.py..."
     python populate_catalog.py
 else
-    echo "La base de datos ya está poblada. Saltando populate_catalog.py."
+    echo "La base de datos ya está poblada."
 fi
